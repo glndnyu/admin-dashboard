e7ae777e4a16e08792020fea66174914b8ed98f2
glndnyu <127914399+glndnyu@users.noreply.github.com>
2023-05-31 14:41:48 +0800

    Finalize style and html of the page

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 const view = document.querySelector(".view")
+const close = document.querySelector(".close")
+const sideBar = document.querySelector(".sidebar");
 
 view.addEventListener("click", toggleSideBar);
 
 function toggleSideBar(event) {
-  const sideBar = document.querySelector(".sidebar");
   sideBar.style.display = "block";
   sideBar.style.position = "fixed";
   sideBar.style.zIndex = "9999";
+
+  close.addEventListener("click", () => {
+    const sideBar = document.querySelector(".sidebar");
+    sideBar.style.display = "none";
+  });
+
+  window.addEventListener("resize", () => {
+    if(window.innerWidth > 1200) {
+      console.log("resized");
+      sideBar.style.display = "block";
+      sideBar.style.position = "fixed";
+      sideBar.style.zIndex = "9999";
+    }
+  });
 }
+
